2c0c735388645f6d6a0b58ab28c7feea5882d8a5
Agon Qurdina <agon.qurdina@gmail.com>
2023-08-16 09:20:38 +0200

    Fix tailwind config after refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,19 @@ module.exports = {
     "./pages/index.js",
     "./comps/App.js",
     "./comps/ConfirmationModal.js",
+    "./comps/Container.js",
+    "./comps/Navbar.js",
     "./comps/Widget.js",
-    "./comps/WidgetBody.js",
-    "./comps/WidgetHeader.js",
-    "./comps/WidgetPayment.js",
-    "./comps/WidgetReceiving.js",
-    "./comps/WidgetShares.js"
+    "./comps/widget/Body.js",
+    "./comps/widget/Header.js",
+    "./comps/widget/Payment.js",
+    "./comps/widget/BankPayment.js",
+    "./comps/widget/CryptoPayment.js",
+    "./comps/widget/Receiving.js",
+    "./comps/widget/Shares.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
